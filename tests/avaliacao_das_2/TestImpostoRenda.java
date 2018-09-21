3c91d560677d21ba432e0508d54273e948f6434c
Lucas Malta <lucas50xd@hotmail.com>
2018-09-21 16:57:44 -0300

    barra vermelha novo teste imposto de renda

--- tests/avaliacao_das_2/TestImpostoRenda.java
+++ tests/avaliacao_das_2/TestImpostoRenda.java
@@ -12,4 +12,11 @@ public class TestImpostoRenda {
 		double calculo = calc.calcularImpostoRenda(1000, 60, 8.5);
 		assertEquals(calculo, 3.14, 0.01);
 	}
+
+	@Test
+	public void testCalculoOutroImpostoRenda() {
+		CalculadoraCDB calc = new CalculadoraCDB();
+		double calculo = calc.calcularImpostoRenda(500, 120, 8.0);
+		assertEquals(calculo, 2.96, 0.01);
+	}
 }
