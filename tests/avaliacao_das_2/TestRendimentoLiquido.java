de415d45d8eaa86f7dcf6c62a08aecf7cf51331b
Lucas Malta <lucas50xd@hotmail.com>
2018-09-21 17:13:48 -0300

    barra vermelha outro teste rendimento liquido

--- tests/avaliacao_das_2/TestRendimentoLiquido.java
+++ tests/avaliacao_das_2/TestRendimentoLiquido.java
@@ -12,4 +12,11 @@ public class TestRendimentoLiquido {
 		double calculo = calc.calcularRendimentoLiquido(1000, 60, 8.5);
 		assertEquals(calculo, 1.0829, 0.01);
 	}
+	
+	@Test
+	public void testOutroCalculoRendimentoLiquido() {
+		CalculadoraCDB calc = new CalculadoraCDB();
+		double calculo = calc.calcularRendimentoLiquido(500, 120, 8.0);
+		assertEquals(calculo, 2.0384, 0.01);
+	}
 }
