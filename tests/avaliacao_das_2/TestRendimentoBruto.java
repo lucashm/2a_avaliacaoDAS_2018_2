9fccc53b7ecd680fe9d007214f289ee23d755a33
Lucas Malta <lucas50xd@hotmail.com>
2018-09-21 16:36:54 -0300

    barra vermelha novo teste rendimento bruto

--- tests/avaliacao_das_2/TestRendimentoBruto.java
+++ tests/avaliacao_das_2/TestRendimentoBruto.java
@@ -8,10 +8,19 @@ import org.junit.Test;
 public class TestRendimentoBruto {
 
 	@Test
-	public void testCadastroUmRendimento() {
+	public void testCadastroUmRendimentoBruto() {
 		CalculadoraCDB calc = new CalculadoraCDB();
 		double resposta = calc.calcularRendimentoBruto(1000, 60, 8.5);
-		
 		assertEquals(13.97, resposta, 0.01);
 	}
+
+	@Test
+	public void testCadastroOutroRendimentoBruto() {
+		CalculadoraCDB calc = new CalculadoraCDB();
+		double outraResposta = calc.calcularRendimentoBruto(500, 120, 8.0);
+		assertEquals(13.15, outraResposta, 0.01);
+	}
 }
+
+
+	
