0c454b33a77629692b3178117b297cdd5ec66a3b
Lucas Malta <lucas50xd@hotmail.com>
2018-09-21 17:19:53 -0300

    triangularização e parametrização rendimento liquido

--- tests/avaliacao_das_2/TestRendimentoLiquido.java
+++ tests/avaliacao_das_2/TestRendimentoLiquido.java
@@ -2,21 +2,44 @@ package avaliacao_das_2;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
 import org.junit.Test;
 
+@RunWith(Parameterized.class)
 public class TestRendimentoLiquido {
 
-	@Test
-	public void testCalculoRendimentoLiquido() {
-		CalculadoraCDB calc = new CalculadoraCDB();
-		double calculo = calc.calcularRendimentoLiquido(1000, 60, 8.5);
-		assertEquals(calculo, 1.0829, 0.01);
+	private double inicial;
+	private int dias;
+	private double taxa;
+	private double resposta;
+	
+	public TestRendimentoLiquido(double inicial, int dias, double taxa, double resposta) {
+		this.inicial = inicial;
+		this.dias = dias;
+		this.taxa = taxa;
+		this.resposta = resposta;
+	}
+	
+	@Parameters
+	public static Collection<Object[]> data() {
+		return Arrays.asList(new Object[][]{
+			{1000, 60, 8.5, 1.0829},
+			{500, 120, 8.0, 2.0384},
+			{3000, 240, 9.0, 4.7342},
+			{400, 550, 8.0, 9.9452}
+		});
 	}
 	
 	@Test
-	public void testOutroCalculoRendimentoLiquido() {
+	public void testCalculoRendimentoLiquido() {
 		CalculadoraCDB calc = new CalculadoraCDB();
-		double calculo = calc.calcularRendimentoLiquido(500, 120, 8.0);
-		assertEquals(calculo, 2.0384, 0.01);
+		double calculo = calc.calcularRendimentoLiquido(inicial, dias, taxa);
+		assertEquals(calculo, resposta, 0.01);
 	}
 }
