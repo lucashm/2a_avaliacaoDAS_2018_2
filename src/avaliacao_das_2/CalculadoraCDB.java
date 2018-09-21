f3b55ab889fac8b352e2d68342b5f0050be2a2c9
Lucas Malta <lucas50xd@hotmail.com>
2018-09-21 16:40:54 -0300

    barra verde implementacao rendimento bruto

--- src/avaliacao_das_2/CalculadoraCDB.java
+++ src/avaliacao_das_2/CalculadoraCDB.java
@@ -5,7 +5,10 @@ public class CalculadoraCDB {
 	public CalculadoraCDB() {};
 	
 	public double calcularRendimentoBruto(double inicial, int dias, double taxa) {
+		double multiplicador = dias/365.0;
+		double taxaReal = taxa*multiplicador;
+		double resposta = inicial * taxaReal / 100.0;
 		
-		return 13.97;
+		return resposta;
 	}
 }
